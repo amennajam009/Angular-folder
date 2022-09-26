be044d4082550aa185538c96675e26d04955087f
trench009 <amennajam7@gmail.com>
2022-09-26 21:22:30 +0500

    add

---
+++ E-Commerce/Front-End/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }, { path: 'mainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'userModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
