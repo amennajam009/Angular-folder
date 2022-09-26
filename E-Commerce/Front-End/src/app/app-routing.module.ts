e4a62e1798f48ade4cdf66f1d1e1f5bb045c77c9
trench009 <amennajam7@gmail.com>
2022-09-26 22:41:44 +0500

    add

--- E-Commerce/Front-End/src/app/app-routing.module.ts
+++ E-Commerce/Front-End/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
   { path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) },
   { path: 'mainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
   { path: 'userModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }]; //indetation....!(to inhence the readability of code)
--- E-Commerce/Front-End/src/app/main-module/main-module.module.ts
+++ E-Commerce/Front-End/src/app/main-module/main-module.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MainModuleRoutingModule } from './main-module-routing.module';
 import { MainModuleComponent } from './main-module.component';
+import { HeaderComponent } from './main-Components/header/header.component';
+import { FooterComponent } from './main-Components/footer/footer.component';
+import { HomeComponent } from './main-Components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    MainModuleComponent
+    MainModuleComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
