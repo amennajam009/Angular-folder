98b5d9d1fcb943c29aa4af2876d5e714614d6573
trench009 <amennajam7@gmail.com>
2022-09-26 22:50:58 +0500

    add

--- E-Commerce/Front-End/src/app/main-module/main-module-routing.module.ts
+++ E-Commerce/Front-End/src/app/main-module/main-module-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main-Components/home/home.component';
 import { MainModuleComponent } from './main-module.component';
 
-const routes: Routes = [{ path: '', component: MainModuleComponent }];
+const routes: Routes = [{ path: '', component: MainModuleComponent,
+children:[
+  {path:'',component:HomeComponent}
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
