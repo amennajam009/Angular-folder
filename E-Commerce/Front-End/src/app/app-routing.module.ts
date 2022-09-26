92f9602e0539bf0d0551a25f01e5cdd3cfa1c3e1
trench009 <amennajam7@gmail.com>
2022-09-26 21:36:55 +0500

    add

--- E-Commerce/Front-End/src/app/app-routing.module.ts
+++ E-Commerce/Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }, { path: 'mainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'userModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }];
+const routes: Routes = [
+  { path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) },
+  { path: 'mainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
+  { path: 'userModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }]; //indetation....!(to inhence the readability of code)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
