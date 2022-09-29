bfa78b69c8749a8fe1c79c6b8a73679062066afe
trench009 <amennajam7@gmail.com>
2022-09-29 22:20:27 +0500

    add

--- E-Commerce/Front-End/src/app/admin-module/admin-module-routing.module.ts
+++ E-Commerce/Front-End/src/app/admin-module/admin-module-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin-Components/dashboard/dashboard.component';
+import { ProductsComponent } from './admin-Components/products/products.component';
 import { AdminModuleComponent } from './admin-module.component';
 
-const routes: Routes = [{ path: '', component: AdminModuleComponent }];
+const routes: Routes = [{ path: '', component: AdminModuleComponent,
+children:[
+  {path:'dashboard', component:DashboardComponent},
+  {path:'products', component:ProductsComponent},
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
