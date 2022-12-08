d6f90e94777520ec02392f43be1e3e8610082141
Amen Najam <98958807+trench009@users.noreply.github.com>
2022-12-09 00:12:35 +0500

    add

--- E-Commerce/Front-End/src/app/app.module.ts
+++ E-Commerce/Front-End/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ToastrModule } from 'ngx-toastr/public_api';
+import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
