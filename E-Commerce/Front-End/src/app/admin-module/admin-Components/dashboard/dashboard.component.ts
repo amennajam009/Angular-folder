8f92f60ac9ba6c168e06ca9dc14dd86a4d18c5e0
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-01-16 22:30:20 +0500

    add

--- E-Commerce/Front-End/src/app/admin-module/admin-Components/dashboard/dashboard.component.ts
+++ E-Commerce/Front-End/src/app/admin-module/admin-Components/dashboard/dashboard.component.ts
@@ -1,12 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart, ChartConfiguration, ChartEvent, ChartType,ChartData } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+  
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  
+  
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
+
+    this.chart?.update();
+  }
   constructor() { }
 
   ngOnInit(): void {
--- E-Commerce/Front-End/src/app/admin-module/admin-module.module.ts
+++ E-Commerce/Front-End/src/app/admin-module/admin-module.module.ts
@@ -1,7 +1,7 @@
 import { TestingComponent } from './admin-Components/testing/testing.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgChartsModule } from 'ng2-charts';
 import { AdminModuleRoutingModule } from './admin-module-routing.module';
 import { AdminModuleComponent } from './admin-module.component';
 import { DashboardComponent } from './admin-Components/dashboard/dashboard.component';
@@ -23,7 +23,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     AdminModuleRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    
+    NgChartsModule,
+   
    
     
     
