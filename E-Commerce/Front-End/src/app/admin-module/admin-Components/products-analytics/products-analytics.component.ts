addc33e5ca07576a20080e9c5d76db6ec6f4482a
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-02-25 17:54:54 +0500

    add

--- E-Commerce/Front-End/src/app/admin-module/admin-Components/products-analytics/products-analytics.component.ts
+++ E-Commerce/Front-End/src/app/admin-module/admin-Components/products-analytics/products-analytics.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsAnalyticsComponent implements OnInit {
 
-  ProductArray=[1,2,3,4,5]
+ProductArray=[1,2,3,4,5];
 
   constructor() { }
 
