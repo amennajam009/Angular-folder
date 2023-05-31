4fef7df4b65cc44f1119f2cd0115f1ed600ad3f5
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-31 16:42:33 +0500

    add

--- E-Commerce/Back-End/controllers/UserManagementController.js
+++ E-Commerce/Back-End/controllers/UserManagementController.js
@@ -5,52 +5,109 @@ const bycrypt=require('bcrypt');
 const jwt=require('jsonwebtoken')
 // accessing my Models here
 
-const UserRegister = async (req,res)=>{
+// const UserRegister3 = async (req,res)=>{
+//     try {
+//         const {firstName,lastName,email,password}=req.body;
+
+//         const checkIFAdminAlreadyExists = await UserManagmentModel.findOne({
+//             Email:email,
+//         })
+//         if(checkIFAdminAlreadyExists?.userPrivilege==='Admin'){
+//             return res.json({
+//                 message: 'Something went wrong!',
+//                 Status: null,
+//                 Data: false
+//             })
+//         }
+
+//         let checkAdminIdentity = email.split('@')[0];
+//         checkAdminIdentity =  checkAdminIdentity.toLowerCase();
+//         if(checkAdminIdentity ==='admin'){
+//             const  adminToCreate =new UserManagmentModel({
+//                 firstName,lastName,email,password,userPrivilege:'Admin'
+//             })
+//             const adminToSave=await adminToCreate.save();
+//             return res.json({
+//                 message:'Register Successfully',
+//                 data:true
+//             })
+//         }
+//         const userResgiteration=new UserManagmentModel({
+//             firstName, lastName, email, password
+        
+//         })
+//         const userToSave =await userResgiteration.save();
+//         res.json({
+//             message:'Register Successfully',
+//             data:true
+//         })
+        
+//          } catch (error) {
+//             res.json({
+//                 error: error.message,
+//                 data: false,
+//                 result: null
+//             })
+//          }
+//         }
+
+
+
+
+
+const UserRegister = async (req, res) => {
     try {
-        const {firstName,lastName,email,password}=req.body;
-
-        const checkIFAdminAlreadyExists = await UserManagmentModel.findOne({
-            Email:email,
-        })
-        if(checkIFAdminAlreadyExists?.userPrivilege==='Admin'){
-            return res.json({
-                message: 'Something went wrong!',
-                Status: null,
-                Data: false
-            })
-        }
-
-        let checkAdminIdentity = email.split('@')[0];
-        checkAdminIdentity =  checkAdminIdentity.toLowerCase();
-        if(checkAdminIdentity ==='admin'){
-            const  adminToCreate =new UserManagmentModel({
-                firstName,lastName,email,password,userPrivilege:'Admin'
-            })
-            const adminToSave=await adminToCreate.save();
-            return res.json({
-                message:'Register Successfully',
-                data:true
-            })
-        }
-        const userResgiteration=new UserManagmentModel({
-            firstName, lastName, email, password
-        
-        })
-        const userToSave =await userResgiteration.save();
-        res.json({
-            message:'Register Successfully',
-            data:true
-        })
-        
-         } catch (error) {
-            res.json({
-                error: error.message,
-                data: false,
-                result: null
-            })
-         }
-        }
-
+      const { firstName, lastName, email, password } = req.body;
+  
+      const checkIfAdminAlreadyExists = await UserManagmentModel.findOne({ email });
+  
+      if (checkIfAdminAlreadyExists?.userPrivilege === 'Admin') {
+        return res.json({
+          message: 'Something went wrong!',
+          status: null,
+          data: false
+        });
+      }
+  
+      if (email === 'amennajam7@gmail.com') { 
+        const adminToCreate = new UserManagmentModel({
+          firstName,
+          lastName,
+          email,
+          password,
+          userPrivilege: 'Admin'
+        });
+  
+        const adminToSave = await adminToCreate.save();
+  
+        return res.json({
+          message: 'Registered as admin successfully',
+          data: true
+        });
+      }
+        const userRegistration = new UserManagmentModel({
+            firstName,
+            lastName,
+            email,
+            password,
+            userPrivilege: 'User'
+          });
+      
+          const userToSave = await userRegistration.save();
+      
+          res.json({
+            message: 'Registered as user successfully',
+            data: true
+          });
+    
+    } catch (error) {
+      res.json({
+        error: error.message,
+        data: false,
+        result: null
+      });
+    }
+  };
        
         const UserLogin = async(req,res) =>{
             try {
